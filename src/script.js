4ecc5a073ed05c9bf38eb32e052f56b994462624
Ripon-Mardy <mardyripon10@gmail.com>
2024-08-01 23:22:19 +0600

    first commit

---
+++ src/script.js
@@ -0,0 +1,4 @@
+ // JavaScript to toggle menu visibility
+ document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.getElementById('menu-items').classList.toggle('hidden');
+});
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+    },    
+    extend: {
+      screens: {        
+        colors: {
+          'custom-gray': '#e2e2e2', // Example custom color
+        },
+        borderColor: theme => ({
+          default: theme('colors.gray.300', 'currentColor'),
+          'custom': theme('colors.custom-gray', 'currentColor'), // Apply custom color
+        }),
+      }
+    },
+    fontSize: {
+      sm: '0.8rem',
+      base: '1.2rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+  },
+  plugins: [],
+}
+
+// content: ["./src/**/*.{html,js}"], * = wildcard
