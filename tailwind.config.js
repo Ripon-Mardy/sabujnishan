71dac6dac2643d32040d0d329fb1abc4b7516b6d
Ripon-Mardy <mardyripon10@gmail.com>
2024-08-02 10:22:26 +0600

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  
   theme: {
     container: {
       center: true,
