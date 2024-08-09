affd2e845529010300ff8417bd8fca6735f14e17
Ripon-Mardy <mardyripon10@gmail.com>
2024-08-09 14:33:06 +0600

    change

--- src/script.js
+++
@@ -1,4 +0,0 @@
- // JavaScript to toggle menu visibility
- document.getElementById('menu-toggle').addEventListener('click', function() {
-    document.getElementById('menu-items').classList.toggle('hidden');
-});
